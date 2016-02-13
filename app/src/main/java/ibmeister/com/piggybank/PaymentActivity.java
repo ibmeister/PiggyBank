9514dd9834d66d6bc4c9d14ca4f224982114d7e2
ibmeister <oyekanibro@yahoo.com>
2016-02-13 14:27:33 -0500

    Layout/implementation updates

--- app/src/main/java/ibmeister/com/piggybank/PaymentActivity.java
+++ app/src/main/java/ibmeister/com/piggybank/PaymentActivity.java
@@ -1,17 +1,32 @@
 package ibmeister.com.piggybank;
 
 
-import com.stripe.android.model.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.stripe.android.model.Card;
 
 
 /**
  * Created by Ibrahim on 1/23/2016.
  */
-public class PaymentActivity{
+public class PaymentActivity extends Activity{
 
-    Card card = new Card("4242-4242-4242-4242", 12, 2017, "123");
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+
+    public void onClickChoosePaymentMethod (View view){
+
+    }
 
 
+   Card card = new Card("4242-4242-4242-4242", 12, 2017, "123");
    boolean numberValidation = card.validateNumber();
    boolean cvcValidation = card.validateCVC();
+
+
 }
